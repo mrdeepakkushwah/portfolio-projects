4ec002a65883fec6f30234abf82072cc9f7eb7c1
Deepak Kushwah <160806589+mrdeepakkushwah@users.noreply.github.com>
2025-08-01 20:31:57 +0530

    SEO buid

--- portfolio-frontend/src/App.jsx
+++ portfolio-frontend/src/App.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Projects from "./pages/Projects";
-import Skills from "./components/SkillBadge";
-import Contact from "./pages/Contact";
-import Achievements from "./pages/Achievements";
+
+// Lazy loaded pages
+const Home = lazy(() => import("./pages/Home"));
+const About = lazy(() => import("./pages/About"));
+const Projects = lazy(() => import("./pages/Projects"));
+const Skills = lazy(() => import("./components/SkillBadge"));
+const Contact = lazy(() => import("./pages/Contact"));
+const Achievements = lazy(() => import("./pages/Achievements"));
 
 const App = () => {
   return (
@@ -15,19 +17,21 @@ const App = () => {
       <div className="min-h-screen flex flex-col">
         <Header />
         <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/skills" element={<Skills />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/achievements" element={<Achievements />} />
-          </Routes>
+          <Suspense fallback={<div className="text-center py-10">Loading...</div>}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/projects" element={<Projects />} />
+              <Route path="/skills" element={<Skills />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/achievements" element={<Achievements />} />
+            </Routes>
+          </Suspense>
         </main>
         <Footer />
       </div>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
--- portfolio-frontend/vite.config.js
+++ portfolio-frontend/vite.config.js
@@ -5,15 +5,17 @@ import sitemap from "vite-plugin-sitemap";
 import pages from "vite-plugin-pages";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(),pages(),
-    sitemap({ hostname: 'https://deepakkhiraofficial.netlify.app' })
+  plugins: [
+    react(),
+    tailwindcss(),
+    pages(),
+    sitemap({ hostname: "https://deepakkhiraofficial.netlify.app" }),
   ],
-  
   server: {
     port: 3000,
   },
   build: {
     outDir: "dist",
   },
-  base: "/", // <- Change this if deploying to GitHub Pages (e.g., '/your-repo-name/')
+  base: "/", // change this for GitHub Pages (e.g., '/repo-name/')
 });
