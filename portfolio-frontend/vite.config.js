29891ff1c9ab8c51feb99a17529213d8c116ab02
Deepak Kushwah <160806589+mrdeepakkushwah@users.noreply.github.com>
2025-08-02 19:20:52 +0530

    updates index.js add some extra meta tag

--- portfolio-frontend/vite.config.js
+++ portfolio-frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from "vite-plugin-sitemap";
 import pages from "vite-plugin-pages";
+import { createHtmlPlugin } from "vite-plugin-html";
 
 export default defineConfig({
   plugins: [
@@ -10,21 +11,56 @@ export default defineConfig({
     tailwindcss(),
     pages(),
     sitemap({ hostname: "https://deepakkhiraofficial.netlify.app" }),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          title: "Deepak Khira Portfolio",
+        },
+        tags: [
+          {
+            tag: "link",
+            attrs: {
+              rel: "preconnect",
+              href: "https://fonts.googleapis.com",
+            },
+            injectTo: "head",
+          },
+          {
+            tag: "link",
+            attrs: {
+              rel: "preconnect",
+              href: "https://fonts.gstatic.com",
+              crossorigin: true,
+            },
+            injectTo: "head",
+          },
+          {
+            tag: "link",
+            attrs: {
+              rel: "preload",
+              as: "image",
+              href: "/deepak.jpg",
+              fetchpriority: "high",
+            },
+            injectTo: "head",
+          },
+        ],
+      },
+    }),
   ],
-  base: "/", // Keep this '/' for Netlify
+  base: "/",
   server: {
     port: 3000,
   },
   build: {
     outDir: "dist",
-    target: "es2015", // Improves compatibility and minifies JS
-    cssCodeSplit: true, // Ensures styles are split per-page
-    sourcemap: false, // Remove source maps for smaller build size
-    minify: "esbuild", // Fast and efficient minifier
+    target: "es2015",
+    cssCodeSplit: true,
+    sourcemap: false,
+    minify: "esbuild",
     rollupOptions: {
       output: {
         manualChunks: {
-          // Split large libraries like react
           react: ["react", "react-dom"],
         },
       },
@@ -32,7 +68,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      "@": "/src", // Optional: clean imports
+      "@": "/src",
     },
   },
 });
